68a0983409655d4a54280cb85d0ab2b65acd25a6
Felix Dollack <felix.dollack@googlemail.com>
2018-10-25 12:13:48 +0900

    Add feedback and controls for beep functionality
    This commit adds keys to control the state of beeping.
    Information which key does what is printed on screen.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -17,12 +17,40 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
     my_blinky->draw();
+    ofSetColor(ofColor::white);
+    ofDrawBitmapString("press spacebar: blinking", 10, 15);
+    ofDrawBitmapString("press o: ON", 10, 30);
+    ofDrawBitmapString("press x: OFF (also blinking)", 10, 45);
+    ofDrawBitmapString("press 0: beep off", 10, 60);
+    ofDrawBitmapString("press 1: beep at ON state", 10, 75);
+    ofDrawBitmapString("press 2: beeps at ON and OFF state ", 10, 90);
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
     if (key == 32) { // space
         my_blinky->setBlinking(!my_blinky->isBlinking());
+    }
+    if (key == 'o') {
+        if (my_blinky->isBlinking() == true) {
+            my_blinky->setBlinking(false);
+        }
+        my_blinky->setBlinkyOn(true);
+    }
+    if (key == 'x') {
+        if (my_blinky->isBlinking() == true) {
+            my_blinky->setBlinking(false);
+        }
+        my_blinky->setBlinkyOn(false);
+    }
+    if (key == 48) {
+        my_blinky->setBeepMode(BeepMode::BEEP_OFF);
+    }
+    if (key == 49) {
+        my_blinky->setBeepMode(BeepMode::BEEP_ON_START);
+    }
+    if (key == 50) {
+        my_blinky->setBeepMode(BeepMode::BEEP_ON_END);
     }
 }
 
